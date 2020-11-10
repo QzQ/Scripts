6ecfe82ff41a731762e93c941a337ae794f5b9b7
QzQ <davydc@sina.com>
2020-11-10 12:43:54 +0800

    添加钉钉打卡

---
+++ dingding.js
@@ -0,0 +1,20 @@
+const $ = new compatibility()
+const point = "dingtalk://dingtalkclient/page/link?url=https://attend.dingtalk.com/attend/index.html';"
+
+$.notify("钉钉", "", "打卡啦", point)
+$done()
+
+function compatibility() {
+    _isQuanX = typeof $task != "undefined"
+    _isLoon = typeof $loon != "undefined"
+    _isSurge = typeof $httpClient != "undefined" && !_isLoon
+    this.read = (key) => {
+        if (_isQuanX) return $prefs.valueForKey(key)
+        if (_isLoon) return $persistentStore.read(key)
+    }
+    this.notify = (title, subtitle, message, url) => {
+        if (_isLoon) $notification.post(title, subtitle, message, url)
+        if (_isQuanX) $notify(title, subtitle, message, { "open-url": url })
+        if (_isSurge) $notification.post(title, subtitle, message, { url: url })
+    }
+}
